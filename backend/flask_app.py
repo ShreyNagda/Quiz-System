b3db947432680f02ab62707fba82512365c0cd1b
Shrey Nagda <shreynagda2714@gmail.com>
2024-03-10 12:05:25 +0530

    Score increment added successfully!

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -27,7 +27,7 @@ def create_room():
     room_code = json['room_code']
     room_name = json['room_name']
     max_players = json['max_players']
-    if check_room(room_code) == False:
+    if checkRoom(room_code) == False:
         room = {
             "room_code": room_code,
             "room_name": room_name,
@@ -46,11 +46,11 @@ def addplayer(room_code):
     player_name = request.json["name"]
     player_id = uuid.uuid4()
     player_score = 0
-    if check_room(room_code):
-        playerlist = get_playerlist(room_code)
+    if checkRoom(room_code):
+        playerlist = getPlayerList(room_code)
         print(playerlist)
         player = {
-            "player_id": player_id,
+            "player_id": str(player_id),
             "player_name": player_name,
             "player_score": player_score
         }
@@ -64,8 +64,8 @@ def addplayer(room_code):
 @app.route("/api/rooms/<room_code>/addquestions", methods=["POST"])
 def addquestions(room_code):
     questions = request.json["questions"]
-    if check_room(room_code):
-        question_list = get_questions(room_code)
+    if checkRoom(room_code):
+        question_list = getQuestions(room_code)
         for i in questions: 
             question_list.append(i)
         rooms_collection.find_one_and_update({"room_code": room_code}, {'$set': {"questions": question_list}})
@@ -73,25 +73,55 @@ def addquestions(room_code):
     else:
         return jsonify({"status":False, "msg": f"Room code {room_code} does not exist!"})
 
+@app.route("/api/rooms/<room_code>/getplayers", methods=["GET"])
+def get_players(room_code):
+    return getPlayerList(room_code)
+
+
+#Check answer and increment score
+@app.route("/api/rooms/<room_code>/checkanswer", methods=["POST"])
+def checkanswer(room_code):
+    if checkRoom(room_code):
+        player_id = request.json["player_id"]
+        question_no = request.json["question_no"]
+        answer = request.json["answer"]
+
+        question = getQuestioByQuestionNumber(room_code, question_no)
+        players  = getPlayerList(room_code)
+        for player in players:
+            if player["player_id"] == player_id: 
+                if question["correct_option"] == answer:
+                    player["player_score"] += 1
+                    rooms_collection.find_one_and_update({"room_code": room_code}, {"$set": {"players": players}})
+                    return jsonify("Correct Answer")
+    else:
+        return jsonify({"status":False, "msg": f"Room code {room_code} does not exist!"})
+
 
 #Check if room exists in the database
-def check_room(room_code):
-    if len(get_room(room_code)) > 0:
+def checkRoom(room_code):
+    if len(getRoom(room_code)) > 0:
         return True
     else:
         return False
 
+
 # Get room with the room code
-def get_room(room_code):
+def getRoom(room_code):
     return list(rooms_collection.find({"room_code": room_code}))[0]
 
 #Get players from the room
-def get_playerlist(room_code):
-    room = get_room(room_code)
+def getPlayerList(room_code):
+    room = getRoom(room_code)
     return room['players']
 
 # Get questions from the room
-def get_questions(room_code):
-    room = get_room(room_code)
+def getQuestions(room_code):
+    room = getRoom(room_code)
     return room["questions"]
 
+def getQuestioByQuestionNumber(room_code, question_no):
+    questions = getQuestions(room_code)
+    for i in questions:
+        if i["question_no"] == question_no:
+            return i
